6c97398e60b012165604315aa79cbe914612d214
xiaoda <lqdai.xiaoda@gmail.com>
2019-08-22 17:19:32 +0800

    Init canvas & draw rivers

--- src/game.js
+++ src/game.js
@@ -8,29 +8,68 @@ class Game {
   }
 
   async init () {
-    const {canvasId} = this.options
-    this.$canvas = $(`#${canvasId}`)
-    this.resizeCanvas()
-    this.stage = new createjs.Stage(canvasId)
     this.cityMap = new CityMap('shanghai')
     this.cityMapData = await this.cityMap.init()
-    this.drawCityMap()
+    this.initCityMap()
 
     $(window).resize(_ => {
-      this.resizeCanvas()
-      this.stage.update()
+      this.initCityMap()
     })
   }
 
-  resizeCanvas () {
+  _transformX (x) {
+    return this.canvasWidth * Math.abs(
+      (x - this.cityMapData.boundary.west) /
+      (this.cityMapData.boundary.west - this.cityMapData.boundary.east)
+    )
+  }
+
+  _transformY (y) {
+    return this.canvasHeight * Math.abs(
+      (y - this.cityMapData.boundary.north) /
+      (this.cityMapData.boundary.north - this.cityMapData.boundary.south)
+    )
+  }
+
+  initCityMap () {
+    const screenWidth = $(window).width()
+    const screenHeight = $(window).height()
+    const screenAspectRatio = screenHeight / screenWidth
+    if (this.cityMapData.aspectRatio > screenAspectRatio) {
+      this.canvasHeight = screenHeight
+      this.canvasWidth = this.canvasHeight * this.cityMapData.aspectRatio ** -1
+    } else {
+      this.canvasWidth = screenWidth
+      this.canvasHeight = this.canvasWidth * this.cityMapData.aspectRatio
+    }
+    this.$canvas = $(`#${this.options.canvasId}`)
     this.$canvas.attr({
-      width: $(window).width(),
-      height: $(window).height()
+      width: this.canvasWidth,
+      height: this.canvasHeight
     })
-  }
+    this.stage = new createjs.Stage(this.options.canvasId)
 
-  drawCityMap () {
-    console.log(this.cityMapData)
+    const cityArea = new createjs.Shape()
+    cityArea.x = cityArea.y = 0
+    cityArea.graphics
+      .beginFill('white')
+      .drawRect(0, 0, this.canvasWidth, this.canvasHeight)
+    this.stage.addChild(cityArea)
+
+    this.cityMapData.rivers.forEach(riverData => {
+      const river = new createjs.Shape()
+      river.graphics
+        .setStrokeStyle(riverData.width, 'round')
+        .beginStroke('#a3dafd')
+      riverData.path.forEach((path, index) => {
+        const x = this._transformX(path[1])
+        const y = this._transformY(path[0])
+        index ? river.graphics.lineTo(x, y) : river.graphics.moveTo(x, y)
+      })
+      river.x = river.y = 0
+      this.stage.addChild(river)
+    })
+    this.stage.update()
   }
 }
 
--- src/map.js
+++ src/map.js
@@ -5,7 +5,23 @@ class CityMap {
 
   async init () {
     this.mapData = (await import(`./map/${this.options.city}.js`)).default
+    this.mapData.origin = this.calculateOrigin(this.mapData.boundary)
+    this.mapData.aspectRatio = this.calculateAspectRatio(this.mapData.boundary)
     return this.mapData
+  }
+
+  calculateOrigin (boundary) {
+    return [
+      (boundary.north + boundary.south) / 2,
+      (boundary.west + boundary.east) / 2
+    ]
+  }
+
+  calculateAspectRatio (boundary) {
+    return Math.abs(
+      (boundary.north - boundary.south) /
+      (boundary.west - boundary.east)
+    )
   }
 }
 
